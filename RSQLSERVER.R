f1e37b136b997fc80d5258842559e02d5302cf28
MariemAB1992 <52740149+MariemAB1992@users.noreply.github.com>
2019-07-10 10:49:45 +0100

    Update RSQLSERVER.R

--- RSQLSERVER.R
+++ RSQLSERVER.R
@@ -1,12 +1,12 @@
 library("RODBC")
 list.of.packages <- c("odbc", "DBI","Rcpp") 
-odbc.key = odbcConnect("name_of data_base_here", uid="yout_login_here", pwd="your_pass_word_here")
+odbc.key = odbcConnect("name_of data_base_here", uid="User name to connect to SQL Server", pwd="Password for the user to connect")
 rawdata = sqlQuery( channel= odbc.key, query = "SELECT * FROM table1")
 con <- dbConnect(odbc::odbc(),
-                 Driver    = "", 
-                 Server    = "",
-                 Database  = "",
-                 UID       = "",
-                 PWD       = "",
+                 Driver    = "SQL Server", 
+                 Server    = "IP address of SQL Serve",
+                 Database  = "name_of data_base",
+                 UID       = "User name to connect to SQL Server",
+                 PWD       = "Password for the user to connect",
                  Port      = 1433)
                  
